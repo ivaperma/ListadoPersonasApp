97030dbe74cdc59864e20c5b3627ec1b5f7068a9
Iván Pérez <79358705+ivaperma@users.noreply.github.com>
2024-01-21 23:11:02 +0100

    Primeros pasos

---
+++ src/main/java/org/example/Persona.java
@@ -0,0 +1,68 @@
+package org.example;
+
+public class Persona {
+    private int id;
+    private String nombre;
+    private String tel;
+    private String email;
+    private static int numeroPersonas = 0;
+
+    public Persona() {
+        this.id = ++Persona.numeroPersonas;
+    }
+
+    public Persona(String nombre, String tel, String email) {
+        this();
+        this.nombre = nombre;
+        this.tel = tel;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", tel='" + tel + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        Persona persona1 = new Persona("Ivan Perez", "666444444", "ivan@gmail.com");
+        System.out.println(persona1);
+    }
+}
+
